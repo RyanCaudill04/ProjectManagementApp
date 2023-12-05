68b5d24ee62b2143745e802e20cfe9cbcb79275b
CyberneticMedic <jccbagley@protonmail.com>
2023-12-05 15:46:43 -0500

    TaskView initializer

--- JavaFX_GUI/src/main/java/demo/TaskViewController.java
+++ JavaFX_GUI/src/main/java/demo/TaskViewController.java
@@ -1,14 +1,16 @@
 package demo;
-import javafx.scene.control.Button;
-
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
+import model.ProjectSystemFACADE;
 
-public class TaskViewController {
+public class TaskViewController implements Initializable{
 
     @FXML
     private Text LabelBody;
@@ -36,8 +38,6 @@ public class TaskViewController {
 
     @FXML
     private Button Projects;
-    
-    
     
     @FXML
     void gotoContactUs(ActionEvent event) throws IOException {
@@ -67,6 +67,29 @@ public class TaskViewController {
     @FXML
     void gotoProjectListView(ActionEvent event) throws IOException {
         App.setRoot("ProjectListView");
+    }
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) { //populates text fields
+        ProjectSystemFACADE facade = ProjectSystemFACADE.getInstance();
+        StringBuffer sb = new StringBuffer();
+        StringBuffer sb2 = new StringBuffer();
+
+        for(int i = 0; i < facade.getTask().getAssignedUsers().size(); i++){
+            sb2.append(facade.getTask().getAssignedUsers().get(i));
+            if(i<facade.getTask().getAssignedUsers().size()){
+                //sb2.append(", ");
+            }
+        }
+        sb.append("Title: " + facade.getTask().getTitle() + "\n");
+        sb.append("Priority: " + String.valueOf(facade.getTask().getPriority()) + "\n");
+        sb.append("Devs: " + sb2.toString() + "\n");
+        sb.append("Due: " + facade.getTask().getDate() + "\n");
+        sb.append("Points: " + String.valueOf(facade.getTask().getPoints()) + "\n");
+        
+        PropertiesBody.setText(sb.toString());
+        UsersBody.setText(sb2.toString());
+        LabelBody.setText(facade.getTask().getTitle());
     }
 
     
