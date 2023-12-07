4fb55e46053e3f1ebfe91bbf5bdd6afd2d3474a1
humphriesgavin1 <humphriesgavin1@gmail.com>
2023-12-07 09:09:04 -0500

    controllers

---
+++ JavaFX_GUI/src/main/java/demo/MyWorkController.java
@@ -0,0 +1,51 @@
+package demo;
+
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MyWorkController {
+
+    @FXML
+    private Button contactButton;
+
+    @FXML
+    private Button homeButton;
+
+    @FXML
+    private Button leaderboardButton;
+
+    @FXML
+    private Button profileButton;
+
+    @FXML
+    private Button projectsButton;
+
+    @FXML
+    void goToHome(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goToLeaderboard(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goToProfile(ActionEvent event) {
+
+    }
+
+    @FXML
+    void gotToContact(ActionEvent event) {
+
+    }
+
+    @FXML
+    void gotToProjects(ActionEvent event) {
+
+    }
+
+}
---
+++ JavaFX_GUI/src/main/java/demo/SignUpController.java
@@ -0,0 +1,30 @@
+package demo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class SignUpController {
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private TextField firstName;
+
+    @FXML
+    private TextField lastName;
+
+    @FXML
+    private TextField password;
+
+    @FXML
+    private Button signUpButton;
+
+    @FXML
+    void signUpUser(ActionEvent event) {
+
+    }
+
+}
